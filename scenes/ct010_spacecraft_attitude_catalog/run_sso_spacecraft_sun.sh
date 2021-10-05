74294c5b8e0e8ba51b3c306756e6ab5e3c44a9ee
Alfonso Gonzalez <alfonso.gonzalez@nablazerolabs.com>
2021-10-05 11:10:17 -0700

    Add Sun attitude pointing spacecraft scene files

---
+++ scenes/ct010_spacecraft_attitude_catalog/run_sso_spacecraft_sun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+${COSMOGRAPHIA} \
+${COSMO_DEMOS}/ct010_spacecraft_attitude_catalog/sso_spacecraft_sun/load.json \
+-p ${COSMO_DEMOS}/ct010_spacecraft_attitude_catalog/sso_spacecraft_sun/script.py  
+
+exit
