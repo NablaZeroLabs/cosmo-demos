346e25f4aa3444c9b95bd1ae25e4bf2f95c51f43
Alfonso Gonzalez <alfonso.gonzalez@nablazerolabs.com>
2021-10-14 11:13:25 -0700

    Move west coast attitude scene files to video 13 directory

--- scenes/ct012_attitude_kernels/run_west_coast_cities.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-${COSMOGRAPHIA} \
-${COSMO_DEMOS}/ct012_attitude_kernels/west_coast_cities/load.json \
--p ${COSMO_DEMOS}/ct012_attitude_kernels/west_coast_cities/script.py  
-
-exit
---
+++ scenes/ct013_west_coast_attitude_kernel/run_west_coast_cities.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+${COSMOGRAPHIA} \
+${COSMO_DEMOS}/ct013_west_coast_attitude_kernel/west_coast_cities/load.json \
+-p ${COSMO_DEMOS}/ct013_west_coast_attitude_kernel/west_coast_cities/script.py  
+
+exit
